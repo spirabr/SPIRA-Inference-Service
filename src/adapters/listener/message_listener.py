f1e21a3078c436cf9e653aea76519e5163e375fd
daitamae <dai.tamae@gmail.com>
2022-08-05 14:26:30 -0300

    Added settings class to hold env variables Added message listener adapter

--- src/adapters/listener/message_listener.py
+++ src/adapters/listener/message_listener.py
@@ -1,23 +1,29 @@
 from asyncio import sleep
 import asyncio
+from core.ports.message_service_port import MessageServicePort
+from core.ports.model_register_port import ModelRegisterPort
 from core.ports.ports import Ports
-
+from core.ports.simple_storage_port import SimpleStoragePort
 from core.services.message_listener_service import (
-    listen_for_messages_and_update,
+    listen_for_messages_and_respond,
     subscribe_to_channel,
 )
+
+
 from settings import Settings
 
 
 async def listen_for_messages_loop(
+    simple_storage_port: SimpleStoragePort,
     message_service_port: MessageServicePort,
+    model_register_port: ModelRegisterPort,
 ):
     """continuously listens for messages (inference requests) and processes the response
 
     Args:
         simple_storage_port (SimpleStoragePort) : port for simple storage
         message_service_port (MessageServicePort) : port for message service
-        database_port (DatabasePort) : port for database
+        model_register_port (ModelRegisterPort) : port for model register
 
     Returns:
         None
@@ -25,7 +31,7 @@ async def listen_for_messages_loop(
     """
     try:
         await subscribe_to_channel(
-            message_service_port, Settings.message_listener_settings.central_channel
+            message_service_port, Settings.message_listener_settings.receiving_channel
         )
     except Exception as e:
         print(e, flush=True)
@@ -36,7 +42,8 @@ async def listen_for_messages_loop(
             await listen_for_messages_and_respond(
                 simple_storage_port,
                 message_service_port,
-                database_port,
+                model_register_port,
+                Settings.message_listener_settings.receiving_channel,
                 Settings.message_listener_settings.central_channel,
             )
         except Exception as e:
@@ -57,6 +64,6 @@ def run_listener(ports: Ports):
         listen_for_messages_loop(
             ports.simple_storage_port,
             ports.message_service_port,
-            ports.database_port,
+            ports.model_register_port,
         )
     )
