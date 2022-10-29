50ea9f77c83f6e6ba97b9e922d43399d4a76f4c4
daitamae <dai.tamae@gmail.com>
2022-10-29 14:33:33 -0300

    Corrects tests.

--- tests/unit_tests/adapters/test_mlflow_adapter.py
+++ tests/unit_tests/adapters/test_mlflow_adapter.py
@@ -5,6 +5,28 @@ from src.core.model.result import ResultUpdate
 
 from tests.mocks.mlflow_mock import MLFlowMock
 
+INFERENCE_JSON_1_WITH_ID = {
+    "id": "fake_inference_id",
+    "gender": "M",
+    "age": 23,
+    "rgh": "fake_rgh",
+    "covid_status": "Sim",
+    "mask_type": "None",
+    "user_id": "507f191e810c19729de860ea",
+    "model_id": "629f994245cda830033cf4cf",
+    "status": "processing",
+    "cid": "fake_cid",
+    "bpm": "fake_bpm",
+    "created_in": "2022-07-18 17:07:16.954632",
+    "respiratory_frequency": "123",
+    "respiratory_insufficiency_status": "Sim",
+    "location": "h1",
+    "last_positive_diagnose_date": "",
+    "hospitalized": "TRUE",
+    "hospitalization_start": "2022-07-18 17:07:16.954632",
+    "hospitalization_end": "2022-07-18 17:07:16.954632",
+    "spo2": "123",
+}
 
 @pytest.fixture
 def adapter_instance():
@@ -16,31 +38,20 @@ def adapter_instance():
 
 def test_predict_method(adapter_instance: MLFlowAdapter):
     inference = Inference(
-        **{
-            "id": "629f815d6abaa3c5e6cf7c16",
-            "sex": "M",
-            "age": 23,
-            "rgh": "fake_rgh",
-            "covid_status": "Sim",
-            "mask_type": "None",
-            "user_id": "507f191e810c19729de860ea",
-            "model_id": "629f992d45cda830033cf4cd",
-            "status": "processing",
-            "created_in": "2022-07-18 17:07:16.954632",
-        },
+        **INFERENCE_JSON_1_WITH_ID,
     )
-    vogal_sustentada = open("tests/mocks/audio_files/audio1.wav", "rb")
-    parlenda_ritmada = open("tests/mocks/audio_files/audio2.wav", "rb")
+    sustentada = open("tests/mocks/audio_files/audio1.wav", "rb")
+    parlenda = open("tests/mocks/audio_files/audio2.wav", "rb")
     frase = open("tests/mocks/audio_files/audio3.wav", "rb")
     aceite = open("tests/mocks/audio_files/audio4.wav", "rb")
 
     inference_files = InferenceFiles(
         aceite=UploadAudio(content=aceite.read(), filename="aceite.wav"),
-        vogal_sustentada=UploadAudio(
-            content=vogal_sustentada.read(), filename="vogal_sustentada.wav"
+        sustentada=UploadAudio(
+            content=sustentada.read(), filename="sustentada.wav"
         ),
-        parlenda_ritmada=UploadAudio(
-            content=parlenda_ritmada.read(), filename="parlenda_ritmada.wav"
+        parlenda=UploadAudio(
+            content=parlenda.read(), filename="parlenda.wav"
         ),
         frase=UploadAudio(content=frase.read(), filename="frase.wav"),
     )
