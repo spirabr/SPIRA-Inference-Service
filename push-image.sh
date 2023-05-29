cc0554e57428d4ad6f3fa33f9539e354da25ce38
Vitor Guidi <vitorguidi@gmail.com>
2023-05-29 11:00:52 -0300

    Forgot to save k8s img build files

--- push-image.sh
+++ push-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker compose stop
-docker compose build inference-service mlflow-server
-docker compose push inference-service mlflow-server
+docker compose build inference-service mlflow-server k8s-test-image
+docker compose push inference-service mlflow-server k8s-test-image
